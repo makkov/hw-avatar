0b45b2f263dc00545c7b5ea19d5237e67be3d52c
maks <maxim_86nva@mail.ru>
2023-04-26 12:57:06 +0200

    init project

---
+++ src/main/java/com/example/hwavatar/HwAvatarApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwavatar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwAvatarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwAvatarApplication.class, args);
+    }
+
+}
